257939fd66c53962f0d275dd6fb2c499d4a9cf12
swarna38 <swarna.sutradhar.info@gamil.com>
2026-09-01 13:09:59 +0600

    banner done with nvbar and footer

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,12 @@
+import { NavLink } from "react-router";
 
 
 const NavBar = () => {
+    const links = <>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to=''>Listed Books</NavLink></li>
+        <li><NavLink to=''>Pages To Read</NavLink></li>
+    </>
     return (
         <div className="navbar bg-base-100 shadow-sm">
         <div className="navbar-start">
@@ -11,32 +17,14 @@ const NavBar = () => {
             <ul
                 tabIndex={-1}
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                <li><a>Item 1</a></li>
-                <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                    <li><a>Submenu 1</a></li>
-                    <li><a>Submenu 2</a></li>
-                </ul>
-                </li>
-                <li><a>Item 3</a></li>
+                {links}
             </ul>
             </div>
-            <a className="btn btn-ghost text-xl">daisyUI</a>
+            <a className="btn btn-ghost text-xl">Book Vibe</a>
         </div>
         <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
-            <li>
-                <details>
-                <summary>Parent</summary>
-                <ul className="p-2 bg-base-100 w-40 z-1">
-                    <li><a>Submenu 1</a></li>
-                    <li><a>Submenu 2</a></li>
-                </ul>
-                </details>
-            </li>
-            <li><a>Item 3</a></li>
+            {links}
             </ul>
         </div>
         <div className="navbar-end">
